776afe670120d981b24ef9d2243a65634f110565
mosesbenjamin <mavewreek@gmail.com>
2020-09-20 10:40:51 +0100

    Callback errors

--- app.js
+++ app.js
@@ -19,7 +19,15 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body)=>{
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].gemoetry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].gemoetry.location.lng}`);
+    if(error){
+        console.log('Unable to connect to google servers')
+    }else if(body.status === 'ZERO_RESULTS'){
+        console.log('Unable to find that address')
+    }else if(body.status ==='OK'){
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].gemoetry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].gemoetry.location.lng}`);
+    }else {
+        console.log('Something went wrong');
+    }
 });
